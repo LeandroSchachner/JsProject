f9ccc7d32f9811b68b03e206e7a4e52077682a2c
RagnarUnknown <87399673+RagnarUnknown@users.noreply.github.com>
2022-04-08 17:38:49 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,52 @@
+ /* GUARDAR DATOS STORAGE */
+function guardarDatos(storage) {
+    let user = document.getElementById('emailAddres').value;
+    let pass = document.getElementById('password').value;
+
+    const usuario = {
+        'user': user,
+        'pass': pass
+    };
+
+    if (storage === 'sessionStorage') {
+        sessionStorage.setItem('user', JSON.stringify(usuario));
+    }
+
+    if (storage === 'localStorage') {
+        localStorage.setItem('user', JSON.stringify(usuario));
+    }
+}
+
+function borrarDatos(storage){
+    storage.clear();
+}
+
+let recordar = document.getElementById("recordarme");
+let btnLogin = document.getElementById("btnLogin");
+let btnVaciarLocalStorage = document.getElementById('btnVaciarLocalStorage');
+let btnVaciarSessionStorage = document.getElementById('btnVaciarSessionStorage');
+
+btnLogin.addEventListener("click", () => {
+    if (recordar.checked) {
+        guardarDatos("localStorage");
+    } else {
+        guardarDatos("sessionStorage");
+    }
+    console.log(btnLogin);
+});
+
+btnVaciarLocalStorage.addEventListener('click', () => {
+    borrarDatos(localStorage);
+
+});
+
+btnVaciarSessionStorage.addEventListener('click', () => {
+    borrarDatos(sessionStorage);
+});
+
+
+
+
+
+
+
